eae718e001a8c13e0470017775092d5af0840797
github-action-benchmark <github@users.noreply.github.com>
2024-04-05 07:48:25 +0000

    add Benchmark (benchmarkjs) benchmark result for d2dbcb06d231eb3b6b70c3ba3e6f704414ed7bf8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712037604908,
+  "lastUpdate": 1712303304877,
   "repoUrl": "https://github.com/aldousalvarez/cactus",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.59%",
             "unit": "ops/sec",
             "extra": "177 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "d2dbcb06d231eb3b6b70c3ba3e6f704414ed7bf8",
+          "message": "build(connector-iroha2): upgrade undici to v5.28.4\n\nBumps [undici](https://github.com/nodejs/undici) from 5.28.3 to 5.28.4.\n- [Release notes](https://github.com/nodejs/undici/releases)\n- [Commits](https://github.com/nodejs/undici/compare/v5.28.3...v5.28.4)\n\n---\nupdated-dependencies:\n- dependency-name: undici\n  dependency-type: direct:production\n...\n\nCo-authored-by: Peter Somogyvari <peter.somogyvari@accenture.com>\n\nSigned-off-by: dependabot[bot] <support@github.com>\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-04-04T10:34:45-07:00",
+          "tree_id": "77c4ae7e184d6ba7656b8234a5dd7056ace0474e",
+          "url": "https://github.com/aldousalvarez/cactus/commit/d2dbcb06d231eb3b6b70c3ba3e6f704414ed7bf8"
+        },
+        "date": 1712303302575,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 586,
+            "range": "±1.55%",
+            "unit": "ops/sec",
+            "extra": "178 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 365,
+            "range": "±1.19%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
           }
         ]
       }
