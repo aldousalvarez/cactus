d4a81dbc6e94efbbc0bbf35739b687ac618bc7be
github-action-benchmark <github@users.noreply.github.com>
2024-05-30 07:35:53 +0000

    add Benchmark (benchmarkjs) benchmark result for 81da3334d8e638f85e398dd228bcef836a278230

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716534265167,
+  "lastUpdate": 1717054553107,
   "repoUrl": "https://github.com/aldousalvarez/cactus",
   "entries": {
     "Benchmark": [
@@ -69,6 +69,44 @@ window.BENCHMARK_DATA = {
             "range": "±2.60%",
             "unit": "ops/sec",
             "extra": "176 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zondervan.v.calvez@accenture.com",
+            "name": "zondervancalvez",
+            "username": "zondervancalvez"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "81da3334d8e638f85e398dd228bcef836a278230",
+          "message": "fix(cmd-api-server): address CVE-2022-25881\n\nPrimary Changes:\n\tUpdated the Dockerfile & https-cache-semantics inside the cmd-api-server package\n\nFixes: #2862\n\nSigned-off-by: zondervancalvez <zondervan.v.calvez@accenture.com>\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-05-29T11:12:31-07:00",
+          "tree_id": "150f754ac29b731c5aafa0fe70fe64afdec1aca2",
+          "url": "https://github.com/aldousalvarez/cactus/commit/81da3334d8e638f85e398dd228bcef836a278230"
+        },
+        "date": 1717054550387,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 564,
+            "range": "±1.69%",
+            "unit": "ops/sec",
+            "extra": "176 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 345,
+            "range": "±1.25%",
+            "unit": "ops/sec",
+            "extra": "180 samples"
           }
         ]
       }
