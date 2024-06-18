4cb16b1b3bb946705c651549de0d13b90a0d1b87
github-action-benchmark <github@users.noreply.github.com>
2024-06-18 06:49:03 +0000

    add Benchmark (benchmarkjs) benchmark result for 092d4501ccf0e43001f2cefb0860b1bcbb0f1485

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718692500527,
+  "lastUpdate": 1718693343763,
   "repoUrl": "https://github.com/aldousalvarez/cactus",
   "entries": {
     "Benchmark": [
@@ -250,6 +250,37 @@ window.BENCHMARK_DATA = {
             "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
             "value": 370,
             "range": "±1.44%",
+            "unit": "ops/sec",
+            "extra": "180 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter.somogyvari@accenture.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "092d4501ccf0e43001f2cefb0860b1bcbb0f1485",
+          "message": "ci(github): add workflow for DCO check\n\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-06-14T08:52:05-07:00",
+          "tree_id": "e59a83c4ef3ce206b28d85053c8bca7119a4c82d",
+          "url": "https://github.com/aldousalvarez/cactus/commit/092d4501ccf0e43001f2cefb0860b1bcbb0f1485"
+        },
+        "date": 1718693342144,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 745,
+            "range": "±2.79%",
             "unit": "ops/sec",
             "extra": "180 samples"
           }
