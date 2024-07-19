2fa6a42f0d8d4de8899005a0954a616576fd5153
github-action-benchmark <github@users.noreply.github.com>
2024-07-19 05:29:16 +0000

    add Benchmark (benchmarkjs) benchmark result for c867a9f5ef084e4e6d7c6f5a641d1dd13f9ce233

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721283398228,
+  "lastUpdate": 1721366956541,
   "repoUrl": "https://github.com/aldousalvarez/cactus",
   "entries": {
     "Benchmark": [
@@ -464,6 +464,44 @@ window.BENCHMARK_DATA = {
             "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
             "value": 364,
             "range": "±1.34%",
+            "unit": "ops/sec",
+            "extra": "182 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michal.bajer@fujitsu.com",
+            "name": "Michal Bajer",
+            "username": "outSH"
+          },
+          "committer": {
+            "email": "9387513+outSH@users.noreply.github.com",
+            "name": "Michal Bajer",
+            "username": "outSH"
+          },
+          "distinct": true,
+          "id": "c867a9f5ef084e4e6d7c6f5a641d1dd13f9ce233",
+          "message": "feat(persistence-fabric): rewrite the plugin\n\n- Rewrite fabric persistence plugin to match persistence ethereum\n  plugin methods and behavior.\n- Change DB schema for persistence.\n- Add tests and sample data to the new plugin. Add them to project CI.\n\nDepends on #3298\n\nSigned-off-by: Michal Bajer <michal.bajer@fujitsu.com>",
+          "timestamp": "2024-07-18T22:52:54+02:00",
+          "tree_id": "0626c0274134b1741d7760598515ec09ec8bf02e",
+          "url": "https://github.com/aldousalvarez/cactus/commit/c867a9f5ef084e4e6d7c6f5a641d1dd13f9ce233"
+        },
+        "date": 1721366954314,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 587,
+            "range": "±1.70%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 362,
+            "range": "±1.32%",
             "unit": "ops/sec",
             "extra": "182 samples"
           }
