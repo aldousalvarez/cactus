92c59ffc34370da086585208c56cc60f3dfa1d79
github-action-benchmark <github@users.noreply.github.com>
2024-09-19 03:14:59 +0000

    add Benchmark (benchmarkjs) benchmark result for 299af74b0e74d5bdb0224fb5a80303ef75024fdb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726219849816,
+  "lastUpdate": 1726715699690,
   "repoUrl": "https://github.com/aldousalvarez/cactus",
   "entries": {
     "Benchmark": [
@@ -983,6 +983,44 @@ window.BENCHMARK_DATA = {
             "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
             "value": 741,
             "range": "±2.98%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter.somogyvari@accenture.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "299af74b0e74d5bdb0224fb5a80303ef75024fdb",
+          "message": "docs(api-server): explain local plugin import through packageSrc config\n\nThis came up during a discussion here and I thought it best to document\nit a little more thoroughly so that later it can be referenced for others\nas well:\nhttps://github.com/hyperledger/cacti/issues/3406#issuecomment-2299654552\n\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-09-17T19:15:15-07:00",
+          "tree_id": "a6698ef08ae1c594584960b0297cf549de9a8ec7",
+          "url": "https://github.com/aldousalvarez/cactus/commit/299af74b0e74d5bdb0224fb5a80303ef75024fdb"
+        },
+        "date": 1726715697588,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 561,
+            "range": "±1.76%",
+            "unit": "ops/sec",
+            "extra": "180 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 670,
+            "range": "±2.89%",
             "unit": "ops/sec",
             "extra": "181 samples"
           }
