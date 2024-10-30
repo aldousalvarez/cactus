903963bc7b54903f450216d970d0866c9718930d
github-action-benchmark <github@users.noreply.github.com>
2024-10-30 07:03:00 +0000

    add Benchmark (benchmarkjs) benchmark result for ff842d2a59ef898e734e1424ee6f26b52ba0af9b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730270949759,
+  "lastUpdate": 1730271780762,
   "repoUrl": "https://github.com/aldousalvarez/cactus",
   "entries": {
     "Benchmark": [
@@ -1406,6 +1406,37 @@ window.BENCHMARK_DATA = {
             "range": "±3.03%",
             "unit": "ops/sec",
             "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter.somogyvari@accenture.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "ff842d2a59ef898e734e1424ee6f26b52ba0af9b",
+          "message": "build: migrate to Typescript target of ES2022 and use Error.cause\n\nProject-wide upgrade to Typescript target of ES2022 so that we can use\nthe new Error APIs.\n\nWherever possible we should now use the new `cause`\nproperty of the built-in `Error` type in combination\nwith the `asError(unknown)` utility function:\n```typescript\nimport { asError } from \"@hyperledger/cactus-common\";\n\ntry {\n    await performSomeImportantOperation();\n} catch (ex: unknown) {\n    const cause = asError(ex);\n    throw new Error(\"Something went wrong while doing something.\", { cause });\n}\n```\nMore information about the EcmaScript proposal that made this possible:\nhttps://github.com/tc39/proposal-error-cause\n\nFixes #3592\n\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-10-24T14:11:50-07:00",
+          "tree_id": "d09b64bb941820b483375a7b716c161a7edb3083",
+          "url": "https://github.com/aldousalvarez/cactus/commit/ff842d2a59ef898e734e1424ee6f26b52ba0af9b"
+        },
+        "date": 1730271778299,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 692,
+            "range": "±2.88%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
           }
         ]
       }
